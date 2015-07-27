b15b996373512f8fe79e3c5df7ef676d5d77b112
nanba <kazuhide-nanba@8clouds.co.jp>
2015-07-27 05:50:43 +0000

    Make a basic User model (including secure passwords)

---
+++ app/models/user_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe User do
+
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+
+  it { should be_valid }  
+end
+end
